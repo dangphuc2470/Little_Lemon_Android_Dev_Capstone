4e01ef56d92496993151554676950ef9737e3aec
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2023-07-24 17:12:52 +0700

    Update

--- app/src/main/java/com/example/littlelemon/ui/screens/onboarding/AuthTemplate.kt
+++ app/src/main/java/com/example/littlelemon/ui/screens/onboarding/AuthTemplate.kt
@@ -12,20 +12,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemoncoursera.R
 import com.example.littlelemon.ui.screens.components.LogoImage
 
 @Composable
-fun AuthTemplate(content: @Composable (ColumnScope.() -> Unit)) {
+fun AuthTemplate(content: @Composable (ColumnScope.() -> Unit))
+{
     Column {
         Box(
             modifier = Modifier
                 .fillMaxWidth(),
             contentAlignment = Alignment.Center
-        ) {
+           ) {
             LogoImage(size = 90.dp)
         }
         Box(
@@ -34,17 +39,37 @@ fun AuthTemplate(content: @Composable (ColumnScope.() -> Unit)) {
                 .height(120.dp)
                 .background(color = MaterialTheme.colorScheme.primary),
             contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = stringResource(R.string.let_s_get_to_know_you),
-                style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onPrimary)
-            )
+           ) {
+            Column() {
+                Text(
+                    text = stringResource(R.string.welcome),
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily(Font(R.font.markazi)),
+                        color = Color(0xFFF4CE14),
+                        fontSize = 40.sp,
+                                                                    ),
+                    modifier = Modifier.padding(top = 2.dp)
+
+                    )
+                Text(
+                    text = "The oldest Mediterranean restaurant in Chicago",
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily(Font(R.font.markazi)),
+                        color = Color(0xFFFFFFFF),
+                        fontSize = 25.sp,
+                        )
+                    )
+            }
+
         }
+
         Text(
             text = stringResource(R.string.personal_information),
             style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
             modifier = Modifier.padding(horizontal = 10.dp, vertical = 15.dp)
-        )
+            )
         content()
 
     }
